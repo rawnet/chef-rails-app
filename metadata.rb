afc8c99871dcd2a8ba005275668f7c2b272384dc
Tom Beynon <tbeynon@rawnet.com>
2013-05-20 15:09:55 +0100

    Depending on MySQL

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,6 @@ description      'Creates Rails application files'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          "build-essential"
+depends          "mysql"
 depends          "database"
 depends          "hostsfile"
