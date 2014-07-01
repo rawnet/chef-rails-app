f58990be7ccbb273b3a543aae3dbe86016402f0e
Daniel Upton <daniel@floppy.co>
2014-07-01 13:56:21 +0100

    Allow configuration of the mysql root credentials

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,7 @@
-default["rails_app"]["apps"] = []
-default["rails_app"]["admin_user"] = "admin"
-default["rails_app"]["rails_user"] = "rails"
+default['rails_app']['apps'] = []
+default['rails_app']['admin_user'] = 'admin'
+default['rails_app']['rails_user'] = 'rails'
+
+default['rails_app']['mysql']['host'] = 'localhost'
+default['rails_app']['mysql']['username'] = 'root'
+default['rails_app']['mysql']['password'] = node['mysql']['server_root_password']
--- recipes/databases.rb
+++ recipes/databases.rb
@@ -1,11 +1,5 @@
 include_recipe 'database::mysql'
 
-mysql_connection_info = {
-  :host     => 'localhost',
-  :username => 'root',
-  :password => node['mysql']['server_root_password']
-}
-
 node_root = node['rails_app']
 
 node_root['apps'].each do |name, app_config|
@@ -15,20 +9,20 @@ node_root['apps'].each do |name, app_config|
     db_config = app_config['environment_config'][environment]['database']
 
     mysql_database db_config['database'] do
-      connection mysql_connection_info
+      connection node_root['mysql']
       action :create
     end
 
     %w[% localhost 127.0.0.1].each do |mysql_host|
       mysql_database_user db_config['user'] do
-        connection mysql_connection_info
+        connection node_root['mysql']
         host mysql_host
         password data_bag[environment]['database_password']
         action :create
       end
 
       mysql_database_user db_config['user'] do
-        connection mysql_connection_info
+        connection node_root['mysql']
         host mysql_host
         database_name db_config['database']
         action :grant
