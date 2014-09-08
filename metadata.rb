c712238521404d345ebe84524c4743967045f102
Daniel Upton <daniel@floppy.co>
2014-09-08 11:48:09 +0100

    Bump to v1.0.13

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tbeynon@rawnet.com'
 license          'All rights reserved'
 description      'Creates Rails application files'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.12'
+version          '1.0.13'
 depends          "build-essential"
 depends          "mysql"
 depends          "database"
