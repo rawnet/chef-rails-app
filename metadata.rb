5066e74bd4abe3226b722338f341158527eef8f0
Daniel Upton <daniel@floppy.co>
2014-10-20 12:23:50 +0100

    Bump to v1.0.17

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tbeynon@rawnet.com'
 license          'All rights reserved'
 description      'Creates Rails application files'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.16'
+version          '1.0.17'
 depends          "build-essential"
 depends          "database"
 depends          "hostsfile"
